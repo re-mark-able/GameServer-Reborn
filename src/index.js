b1cadf3458768092354403c0105ffd5ed9a6c0e1
Mark <belstead@iinet.net.au>
2025-01-22 12:17:45 +0800

    basic web frontend

--- src/index.js
+++ src/index.js
@@ -6,37 +6,40 @@ import compression from "compression";
 import { debugWithTime } from "./util/debugUtil.js";
 
 import config from "../config.json" with { type: "json" };
-
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import fs from "fs";
 
 import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(
-      config.dataDirectory + "/users.db",
-      sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, // Make the database file if it doesn't already exist
-      (error) => {
-        if (error) {
-          console.error("[index.js] Error opening database:", error.message);
-        }
-      }
+  config.dataDirectory + "/users.db",
+  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, // Make the database file if it doesn't already exist
+  (error) => {
+    if (error) {
+      console.error("[index.js] Error opening database:", error.message);
+    }
+  }
 );
 
 const app = express();
 const PORT = process.env.LISTEN_PORT || config.listenPort;
 
-const QUERY = "CREATE TABLE IF NOT EXISTS UserData (MayhemId int unique, UserId int unique, UserAccessToken string, UserAccessCode string, UserRefreshToken string, SessionId string unique, SessionKey string unique, WholeLandToken string, LandSavePath string, CurrencySavePath string);";
-await db.run(QUERY, async (error) => { // Make sure database is initialized
-	if (error) {
-		console.error("Could not initialize database: ", error.message)
-		return;
-	}
+const QUERY =
+  "CREATE TABLE IF NOT EXISTS UserData (MayhemId int unique, UserId int unique, UserAccessToken string, UserAccessCode string, UserRefreshToken string, SessionId string unique, SessionKey string unique, WholeLandToken string, LandSavePath string, CurrencySavePath string);";
+await db.run(QUERY, async (error) => {
+  // Make sure database is initialized
+  if (error) {
+    console.error("Could not initialize database: ", error.message);
+    return;
+  }
 });
 
 db.close((error) => {
-          if (error) {
-            console.error("Error closing database:", error.message);
-            return;
-          }
+  if (error) {
+    console.error("Error closing database:", error.message);
+    return;
+  }
 });
 
 app.use((req, res, next) => {
@@ -50,14 +53,19 @@ app.use(
       return true; // Always compress the response
     },
     threshold: 0,
-  }),
+  })
 );
 app.use(routes);
-
+app.use("/", express.static("public"));
+/*
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  //let path = require("path");
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  console.log(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
-
+*/
 app.use((req, res) => {
   res.status(404).send("Do'h! Error 404");
 });
